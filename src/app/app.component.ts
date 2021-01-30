c9676be31ce88b0bd562609f54e8a845be12e068
Burak <bgokselburak@gmail.com>
2021-01-30 04:02:27 +0300

    implement dummy data using dummy Product interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { Product } from './models/product.model';
+import { ProductDataService } from './services/data/product.data.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  items: any;
-  constructor(private firestore: AngularFirestore) {
-    this.items = firestore.collection('items').valueChanges();
+  items!: Product[];
+  constructor(private productDataService: ProductDataService) {
+    this.productDataService.getAll().subscribe(result => {
+      this.items = result;
+    });
+    this.productDataService
+      .create({
+        id: 'asd',
+        name: 'New Product',
+        quantity: 5
+      })
+      .subscribe(console.log);
   }
 }
