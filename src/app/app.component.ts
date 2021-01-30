33ca7443b41591f29c47b724d39e68cd699d11b1
Burak <bgokselburak@gmail.com>
2021-01-30 18:30:01 +0300

    do not add any logic in app level reserve as entrypoint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,29 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
 import { Product } from './models/product.model';
-import { ProductDataService } from './services/data/product.data.service';
+
 @Component({
-  selector: 'app-root',
+  selector: 'app-pe-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   items!: Product[];
-  constructor(private productDataService: ProductDataService) {
-    this.productDataService.getAll().subscribe(result => {
-      this.items = result;
-    });
-    this.productDataService
-      .create({
-        id: 'asd',
-        name: 'New Product',
-        quantity: 5
-      })
-      .subscribe(console.log);
+  constructor(
+    // private productDataService: ProductDataService, 
+    // public auth: AngularFireAuth
+    ) {
+    // this.productDataService.getAll().subscribe(result => {
+    //   this.items = result;
+    // });
+    // this.productDataService
+    //   .create({
+    //     id: 'asd',
+    //     name: 'New Product',
+    //     quantity: 5
+    //   })
+    //   .subscribe(console.log);
+    // setTimeout(() => {
+    //   this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    // }, 5000);
   }
 }
