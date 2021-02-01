3859b5efd3dde7030c0d61f6d17f815854bdda95
Burak <bgokselburak@gmail.com>
2021-02-01 04:28:48 +0300

    redirect after login

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { StandardMessages } from 'src/app/enums';
 import { NotificationTypes } from 'src/app/models/notification-types.enum';
 import { AuthService } from 'src/app/services/auth/auth.service';
@@ -12,7 +13,12 @@ import { NotificationService } from 'src/app/services/utils/notification.service
 export class LoginComponent {
   public formGroup!: FormGroup;
 
-  constructor(private fb: FormBuilder, private authService: AuthService, private notificationService: NotificationService) {
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private notificationService: NotificationService,
+    private router: Router
+  ) {
     this.createForm();
   }
 
@@ -30,7 +36,11 @@ export class LoginComponent {
         detail: StandardMessages.ValidationError
       });
     } else {
-      this.authService.login(this.formGroup.value);
+      this.authService.login(this.formGroup.value).subscribe(result => {
+        if (result) {
+          this.router.navigateByUrl('/');
+        }
+      });
     }
   }
 }
