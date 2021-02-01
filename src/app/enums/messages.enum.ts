c305de956249e852d936fc13b563bdee96b3102a
Burak <bgokselburak@gmail.com>
2021-02-01 04:28:24 +0300

    add messages

--- src/app/enums/messages.enum.ts
+++ src/app/enums/messages.enum.ts
@@ -1,5 +1,8 @@
 export enum StandardMessages {
   FormError = 'Values are not valid.',
   ValidationError = 'Please enter valid info.',
-  LoggedIn = 'Logged in successfully.'
+  LoggedIn = 'Logged in successfully.',
+  Registered = 'Registered in successfully.',
+  Error = 'There was a problem, please try again.',
+  UserNotFound = 'User not found, please check credentials.'
 }
